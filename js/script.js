45810de15911daffcc5694411ee04106975376fe
Crescenzo De Martino <cresckamina@gmail.com>
2020-05-20 16:27:25 +0200

    Aggiunto messaggio di errore nel caso di valori non numerici

--- js/script.js
+++ js/script.js
@@ -1,25 +1,28 @@
-// Fine Sezione Calcoli
+// Fine Sezione Calcoli e Condizioni
 var numeroChilometri = parseInt ( prompt ("Quanti chilometri devi percorrere?" ) );
 
 var eta = parseInt ( prompt ("Quanti anni hai?" ) );
 
 var prezzoIniziale = ( numeroChilometri * 0.21 );
 
-if (eta < 18 ) {
-  sconto = prezzoIniziale * 0.2
-}
+if ( !isNaN(numeroChilometri) && !isNaN(eta)) {
+  if (eta < 18 ) {
+    sconto = prezzoIniziale * 0.2
+  }
 
-if (eta > 65 ) {
-  sconto = prezzoIniziale * 0.4
-}
+  if (eta > 65 ) {
+    sconto = prezzoIniziale * 0.4
+  }
 
-else if (eta >= 18 && eta <= 65 ) {
-  sconto = 0
+  else if (eta >= 18 && eta <= 65 ) {
+    sconto = 0
+  }
+} else {
+  alert("ERRORE! Controlla i tuoi dati e riprova")
 }
 
 var prezzoFinale = ( prezzoIniziale - sconto )
-
-// Fine Sezione Calcoli
+// Fine Sezione Calcoli e Condizioni
 
 // Sezione trascrizione
 document.getElementById('chilometri').innerHTML = numeroChilometri;
